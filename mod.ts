f3481fe6d6bfcb3f0820e3610c3fc4e232ed93ef
Nikita <nikita.voloboev@gmail.com>
2024-04-06 19:12:27 +0400

    .

--- mod.ts
+++ mod.ts
@@ -1,5 +1,1 @@
-// TODO: below is not used actually,
-// figure out how how publishing to both JSR and NPM works
-// need to figure out .github/workflows/release.yml
-
-// export * from "./src/other.ts"
+export * from "./src/clipboard.ts"
