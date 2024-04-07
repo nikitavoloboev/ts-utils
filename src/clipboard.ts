6541c5d4b0d20e2a182b414c89f061720f4d86c0
Nikita <nikita.voloboev@gmail.com>
2024-04-07 14:32:51 +0400

    .

--- src/clipboard.ts
+++ src/clipboard.ts
@@ -1,12 +1,12 @@
 import clipboard from "clipboardy"
 
-export function getClipboard() {
+export function getClipboard(): string {
   return clipboard.readSync()
 }
 
 export function appendToClipboard(
   newClipboard: string | object | Array<unknown>,
-) {
+): void {
   if (typeof newClipboard === "object" || Array.isArray(newClipboard)) {
     clipboard.writeSync(JSON.stringify(newClipboard))
     return
