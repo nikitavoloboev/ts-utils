46c54fabd9523e56a93c4c1c16f22f6f06fe34fe
Nikita <github@nikiv.dev>
2024-11-17 00:07:45 +0100

    .

---
+++ mod.ts
@@ -0,0 +1,5 @@
+export * from "./src/clipboard.ts"
+export * from "./src/date.ts"
+export * from "./src/file.ts"
+export * from "./src/jxa.ts"
+export * from "./src/safari.ts"
---
+++ readme.md
@@ -0,0 +1,1 @@
+Trying to get a clean version of old [ts-utils](https://github.com/nikitavoloboev/ts-test/tree/main/ts-utils) up.
